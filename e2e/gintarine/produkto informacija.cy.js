51ee1ac8bb14d2bcd137d7dba3cc53b6a4805827
lukastrop <109535779+lukastrop@users.noreply.github.com>
2022-07-25 12:48:15 +0300

    Add files via upload

--- e2e/gintarine/produkto informacija.cy.js
+++ e2e/gintarine/produkto informacija.cy.js
@@ -1,41 +1,40 @@
 // <reference types="cypress" />
 
 describe("Ar produkto puslapyje matoma visa reikalinga informacija", () => {
-  it("Tikrinam laukelius", () => {
-      cy.visit("https://www.gintarine.lt/acc-long-600-mg-snypsciosios-tabletes-n10");
-      cy.wait(1000)
-      
-      // Pristatymo laikas, kategorija, informacinis lapelis, kaina, nuotrauka
-      cy.get('#main-product-img-21025').should('be.visible');
-      cy.get('.price-val').should('be.visible');
-      cy.get('.single-product__info-icons > :nth-child(2)').scrollIntoView().should('be.visible');
-      
+  it("Testing required fields", () => {
+    cy.visit("https://www.gintarine.lt/acc-long-600-mg-snypsciosios-tabletes-n10");
+    cy.wait(1000)
+
+    // category,  product image etc
+    cy.get('#main-product-img-21025').should('be.visible');
+    cy.get('.price-val').should('be.visible');
+    cy.get('.single-product__info-icons > :nth-child(2)').scrollIntoView().should('be.visible');
+
 
 
 
   });
 
-  it("Jeigu tai nereceptinis vaistas", () => {
-      if (cy.get('.single-product__info-label').scrollIntoView().should('be.visible')) {
-        //turi būti nerp info
+  it("If product is OTC", () => {
+    if (cy.get('.single-product__info-label').scrollIntoView().should('be.visible')) {
+      //There should be OTC info label
       cy.get('.single-product__left > .single-product__details > .product-accordion > .single-product__details-description > .accordion__toggle').click();
       cy.get('.single-product__left > .single-product__details > .product-accordion').contains('informacinis lapelis', { matchCase: false });
       cy.get('.single-product__info-message').contains('nereceptinis vaistas', { matchCase: false });
-      }
-      
-    });
+    }
+
+  });
 
 
-  it("Jeigu tai maisto papildas", () => {
-      cy.visit("https://www.gintarine.lt/elevit-breastfeeding-30-kapsuliu")
-      if (cy.get('.single-product__info-label').should('not.exist'))
-      {
-        //turi būti nerp info
+  it("If product is food supplement", () => {
+    cy.visit("https://www.gintarine.lt/elevit-breastfeeding-30-kapsuliu")
+    if (cy.get('.single-product__info-label').should('not.exist')) {
+      //Should contain supplement info
       cy.get('.single-product__left > .single-product__details > .product-accordion > .single-product__details-description > .accordion__toggle').click();
       cy.get('.single-product__left > .single-product__details > .product-accordion > .single-product__details-description > .accordion__content').contains('maisto papildas', { matchCase: false });
       cy.get('.single-product__info-message').contains('maisto papildai', { matchCase: false });
-      
-      }
-      
-    });
-  })  
+
+    }
+
+  });
+})  
