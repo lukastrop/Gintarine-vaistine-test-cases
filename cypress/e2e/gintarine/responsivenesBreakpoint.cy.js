c9127a8af457eebf7c98660d8e865d3d5a1bd2b5
lukastrop <109535779+lukastrop@users.noreply.github.com>
2022-11-03 10:56:39 +0200

    Update responsivenesBreakpoint.cy.js

--- cypress/e2e/gintarine/responsivenesBreakpoint.cy.js
+++ cypress/e2e/gintarine/responsivenesBreakpoint.cy.js
@@ -1,15 +1,8 @@
-// <reference types="cypress" />
-
-
 describe("responsiveness breakpoint test", () => {
   it("the css grid template changes the number of columns", function () {
     //Desktop version 3 columns by default
     cy.visit("https://www.gintarine.lt/akcijos-4")
 
-    // Cypress does not recognize the selector "p::before"
-    // because the jQuery engine does not support them
-    // so we get the content through the computed style
-    // see https://codepen.io/chriscoyier/pen/Pzzawj
     cy.window().then((win) => {
       cy.get('.category-products__cards').then(($el) => {
             .filter((k, el) => {
